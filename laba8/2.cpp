a0dfb68c7a752534e90098bc1f512a3f523fd13b
Sofya <SKShirokaja@stud.kantiana.ru>
2021-11-18 12:28:12 +0200

    lab8 added

---
+++ laba8/2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int FirstNumber(int x) {
+    int num = x;
+    while (num / 10 > 0) {
+        num = num / 10;
+    }
+    return num;
+}
+
+int MaxNumber(int x) {
+    int max = 0;
+    int k = x;
+    while (k > 0) {
+        if (max < k % 10) max = k % 10;
+        k = k / 10;
+    }
+    return max;
+}
+
+int main()
+{
+    int n;
+    ifstream in("input.txt");
+    in >> n;
+    int* mas = new int[n];
+    for (int i = 0; i < n; ++i) {
+        in >> mas[i];
+    }
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (FirstNumber(mas[i]) > FirstNumber(mas[j])) swap(mas[i], mas[j]);
+            else if ((FirstNumber(mas[i]) == FirstNumber(mas[j])) && (MaxNumber(mas[i]) > MaxNumber(mas[j]))) swap(mas[i], mas[j]);
+            else if (((FirstNumber(mas[i]) == FirstNumber(mas[j])) && (MaxNumber(mas[i]) == MaxNumber(mas[j]))) && (mas[i] > mas[j])) swap(mas[i], mas[j]);
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        cout << mas[i] << " ";
+    }
+    delete[] mas;
+    return 0;
+}
---
+++ laba8/3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream> 
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	ifstream in("input.txt");
+	int i, j;
+	in >> i >> j;
+	int** matrix = new int* [i];
+	for (int count = 0; count < i; count++)
+		matrix[count] = new int[j];
+	for (int k = 0; k < i; ++k) {
+		for (int l = 0; l < j; ++l) {
+			in >> matrix[k][l];
+		}
+	}
+	int min_column = 0;
+	int min = matrix[0][0];
+	for (int k = 0; k < i; ++k) {
+		for (int l = 0; l < j; ++l) {
+			if (min > matrix[k][l]) {
+				min = matrix[k][l];
+				min_column = l;
+			}
+			if (matrix[k][l] < 0) matrix[k][l] = 0;
+		}
+	}
+	for (int k = 0; k < i; ++k) {
+		for (int l = 0; l < j; ++l) {
+			cout << matrix[k][l] << " ";
+		}
+		cout << endl;
+	}
+	delete[] matrix;
+	cout << "min number's column: " << min_column + 1;
+	return 0;
+}
